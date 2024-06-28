a3a1cad5579711e6ff1cc591a51ddc9cbb9c66ec
gahyeon <gimgahyeon12@gmail.com>
2024-06-28 18:51:05 +0900

    백엔드

---
+++ GalleryProject/posts/serializers.py
@@ -0,0 +1,10 @@
+# from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import Post
+
+class PostSerializer(ModelSerializer):
+    # author = serializers.ReadOnlyField(source = 'author.username')
+
+    class Meta:
+        model = Post
+        fields = [ 'id', 'title', 'content', 'author' ]
---
+++ GalleryProject/withmusic/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import Music
+
+class MusicSerializer(ModelSerializer):
+    writer = serializers.ReadOnlyField(source = 'writer.username')
+
+    class Meta: 
+        model = Music
+        fields = ['id', 'name', 'artist', 'content', 'writer']
